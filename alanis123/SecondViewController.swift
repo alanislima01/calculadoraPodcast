bd0a9fb503e9f9ded971228c46a2d4fa997dd314
Alanis Lima Santa Clara <alanis@br-ufpe-01.local>
2020-03-19 11:27:26 -0300

    primeiro commit

---
+++ alanis123/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  alanis123
+//
+//  Created by Alanis Lima Santa Clara on 17/03/20.
+//  Copyright © 2020 Alanis Lima Santa Clara. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
